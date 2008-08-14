88760b76498bb0d4f1b976f45a23841904615794
Hugo Dworak <hugo.dworak+gsoc2008@example.com>
2008-08-14 23:06:28 +0200

    Do not allow users to open local files.

--- validator/controllers/validation.py
+++ validator/controllers/validation.py
@@ -1,6 +1,6 @@
 import logging
 import sys
-import urllib
+import urllib, re
 
 from validator.lib.base import *
 from libvalidator import libvalidator
@@ -28,9 +28,13 @@ class ValidationController(BaseController):
         return render('validation.result')
         
     def parseOnlineDocument(self):
+        reHyperlink = re.compile('^(?:data:|((ftp|gopher|https?)://))\S+$', re.IGNORECASE)
         try:
-            remote = urllib.urlopen(request.POST['input_remote'])
-            c.result = self.parser.parse(remote.read(), location=request.POST['input_remote'], headers=remote.info())
+            if reHyperlink.search(request.POST['input_remote']) is not None:
+                remote = urllib.urlopen(request.POST['input_remote'])
+                c.result = self.parser.parse(remote.read(), location=request.POST['input_remote'], headers=remote.info())
+            else:
+                raise 'Unsupported Uniform Resource Identifier.', ''
         except:
             c.error = '%s %s' % (sys.exc_info()[0], sys.exc_info()[1])
             return render('validation.error')
