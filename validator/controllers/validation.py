1f5a66d072dd13cb57636a2a0e03cfe176b46f60
Hugo Dworak <hugo.dworak+gsoc2008@example.com>
2008-08-14 03:20:59 +0200

    Parsing done using libvalidator. Templates for the results and errors.

--- validator/controllers/validation.py
+++ validator/controllers/validation.py
@@ -1,64 +1,43 @@
 import logging
 import urllib
-import encutils
 
 from validator.lib.base import *
-from validator.lib.parser import *
+from libvalidator import libvalidator
 
 log = logging.getLogger(__name__)
 
 class ValidationController(BaseController):
 
     def __init__(self):
-        self.parser = Parser()
+        self.parser = libvalidator()
+        pass
 
     def index(self):
         return render('validation.index')
     
     def parseDirectInput(self):
-        self.parser.parse(request.POST['input'])
+        try:
+            c.result = self.parser.parse(request.POST['input_direct'])
+        except:
+            return render('validation.error')            
+        return render('validation.result')
         
     def parseOnlineDocument(self):
         try:
-            remote = urllib.urlopen(request.POST['input'])
-            self.parser.parse(remote.read(), header=remote.info())
-        except IOError:
-            pass
-    
+            remote = urllib.urlopen(request.POST['input_remote'])
+            c.result = self.parser.parse(remote.read(), location=request.POST['input_remote'], headers=remote.info())
+        except IOError, err:
+            c.error = err
+            return render('validation.error')
+        except:
+            return render('validation.error')
+        return render('validation.result')
+        
     def parseUploadedFile(self):
-        self.parser.parse(request.POST['input'].value)
-    
-        """
-        c.encoding = None
-        if 'input' in request.POST and request.POST['input'].strip() != '':
-            c.code = request.POST['input']
-            try:
-                c.encoding = encutils.getEncodingInfo(text=c.code)
-            except HTMLParseError:
-                c.error = "Error parsing the directly input code."
-        elif 'link' in request.POST and request.POST['link'].strip() != '':
-            try:
-                c.code = urllib.urlopen(request.POST['link']).read()
-                c.encoding = encutils.getEncodingInfo(url=request.POST['link'])
-            except IOError, e:
-                c.error = e[1]
-            except HTMLParseError:
-                c.error = "Error parsing the remote document."
-        elif 'file' in request.POST:
-            c.code = request.POST['file'].value
-            try:
-                c.encoding = encutils.getEncodingInfo(text=c.code)
-            except HTMLParseError:
-                c.error = "Error parsing the upload file."
-        
-        if str(c.encoding) != '':
-            try:
-                c.code = unicode(c.code, str(c.encoding), errors='strict')
-            except TypeError:
-                c.code
-        else:
-            c.encoding = repr(c.encoding) #'Unicode'
-        
-        return render('dummy.index')
-        """
-        
+        #print request.POST['input_upload'].value
+        #c.result = self.parser.parse(request.POST['input_upload'].value)
+        try:
+            c.result = self.parser.parse(request.POST['input_upload'].value)
+        except:
+            return render('validation.error')
+        return render('validation.result')
